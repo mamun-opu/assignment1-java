19748ab678b60f8c745856c09fc2602e74f76a1f
mamun-opu <abmamun504@gmail.com>
2023-10-20 19:18:40 -0400

    constructed three points with class point

--- src/Geometry.java
+++ src/Geometry.java
@@ -1,13 +1,31 @@
-public class Geometry {
-    public static void main(String[] args){
-        System.out.println("hello world");
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Point {
+    double x;
+    double y;
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
-}
-
-class Triangle{
 
 }
 
-class Point{
+public class Geometry {
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        Point[] vertices = new Point[3];
+
+        for (int j = 0; j < 3; j++) {
+            System.out.print("Construction of a new point ");
+            System.out.print("Please enter x: ");
+            double x = scanner.nextDouble();
+            System.out.print("Please enter y: ");
+            double y = scanner.nextDouble();
+            vertices[j] = new Point(x, y);
+            System.out.println(vertices[j].x);
+            System.out.println(vertices[j].y);
+        }
+    }
 }
